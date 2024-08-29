933cb76b58726d4f993fe449ece923eea842fc8d
Gabriel_Ribeiro <gaberds2009@gmail.com>
2024-08-29 16:15:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let idade = 15
+let idadeString = idade.toString()
+console.log(`Valor da primeira: ${idade}, Tipo: ${typeof idade}`)
+console.log(`Valor da segunda: ${idadeString}, Tipo: ${typeof idadeString}`)
